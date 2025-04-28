cc9369066bafc45d639b67356643fef765c18cbd
ramDev <ramprajapati7892@gmail.com>
2025-04-28 11:20:11 +0530

    new update

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,13 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use('/api/auth', require('./routes/authRoutes'));
 
-app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
+// app.listen(process.env.PORT, () =>
+//       console.log(`Server running on port ${process.env.PORT}`)
+//     );
